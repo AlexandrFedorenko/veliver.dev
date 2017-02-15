426de693cde7ecb9fcdbc3aad3c79a98cff79957
alexandrfedorenko <alexandrfedorenkoooo@gmail.com>
2017-02-15 12:07:15 +0200

    v 0.2

--- js/common.js
+++ js/common.js
@@ -1,12 +1,12 @@
 $(document).ready(function() {
-	// Resize bg
-	function heightDetect() {
-		$(".main_head").css("height", $(window).height());
-	};
-	heightDetect();
-	$(window).resize(function() {
-		heightDetect();
-	});
+	// // Resize bg
+	// function heightDetect() {
+	// 	$(".main_head").css("height", $(window).height());
+	// };
+	// heightDetect();
+	// $(window).resize(function() {
+	// 	heightDetect();
+	// });
 //languages ​​switch
 	$("#en").click(function(){
 		$(this).addClass("active");
@@ -17,7 +17,223 @@ $(document).ready(function() {
 		$("#en").removeClass('active');
 	});
 
+	//slider-1
+	$(".slid1").addClass('act');//Startup give class
+	$(".slid1").click(function(){ // проверяем на наличие класса у всех слайдов и оставляем активными только 1 слайд / We check for the presence of a class of all slides and leave active only 1 slide
+		$(this).addClass("act");
+		$(".slid2").removeClass('act');
+		$(".slid3").removeClass('act');
+		$(".green2").removeClass('activegrin');
+		$(".green3").removeClass('activegrin');
+		$("#expresBtn").removeClass('activegr');
+		$(".blue2").removeClass('activegrin');
+		$(".blue3").removeClass('activegrin');
+		$("#standartBtn").removeClass('activegr');
+		$(".red2").removeClass('activegrin');
+		$(".red3").removeClass('activegrin');
+		$("#vipBtn").removeClass('activegr');
+		$(".orang2").removeClass('activegrin');
+		$(".orang3").removeClass('activegrin');
+		$("#poslestroyBtn").removeClass('activegr');
 
+
+	});
+	$(".slid2").click(function(){
+		$(this).addClass("act");
+		$(".slid1").removeClass('act');
+		$(".slid3").removeClass('act');
+		$(".green").removeClass('activegrin');
+		$(".green3").removeClass('activegrin');
+		$("#expresBtn").removeClass('activegr');
+		$(".blue").removeClass('activegrin');
+		$(".blue3").removeClass('activegrin');
+		$("#standartBtn").removeClass('activegr');
+		$(".red").removeClass('activegrin');
+		$(".red3").removeClass('activegrin');
+		$("#vipBtn").removeClass('activegr');
+		$(".orang").removeClass('activegrin');
+		$(".orang3").removeClass('activegrin');
+		$("#poslestroyBtn").removeClass('activegr');
+	});
+	$(".slid3").click(function(){
+		$(this).addClass("act");
+		$(".slid2").removeClass('act');
+		$(".slid1").removeClass('act');
+		$(".green2").removeClass('activegrin');
+		$(".green").removeClass('activegrin');
+		$("#expresBtn").removeClass('activegr');
+		$(".blue2").removeClass('activegrin');
+		$(".blue").removeClass('activegrin');
+		$("#standartBtn").removeClass('activegr');
+		$(".red2").removeClass('activegrin');
+		$(".red").removeClass('activegrin');
+		$("#vipBtn").removeClass('activegr');
+		$(".orang2").removeClass('activegrin');
+		$(".orang").removeClass('activegrin');
+		$("#poslestroyBtn").removeClass('activegr');
+	});
+	$("#expresBtn").click(function(){  // при клике на кнопку активировать определленые кнопки  и убрать не нужные или добавляем нужные /	when you click on the button to activate opredellenye button and remove unnecessary or add the desired
+		$(this).toggleClass("activegr");
+		if($(".slid1").hasClass("act")){
+			$(".green").toggleClass("activegrin");
+			$(".green2").removeClass('activegrin');
+			$(".green3").removeClass('activegrin');
+			$(".orang").removeClass('activegrin');
+			$(".orang2").removeClass('activegrin');
+			$(".orang3").removeClass('activegrin');
+			$("#poslestroyBtn").removeClass('activegr');
+		}else if($(".slid2").hasClass("act")){
+			$(".green2").toggleClass("activegrin");
+			$(".green").removeClass('activegrin');
+			$(".green3").removeClass('activegrin');
+			$(".orang").removeClass('activegrin');
+			$(".orang2").removeClass('activegrin');
+			$(".orang3").removeClass('activegrin');
+			$("#poslestroyBtn").removeClass('activegr');
+		}else if($(".slid3").hasClass("act")){
+			$(".green3").toggleClass("activegrin");
+			$(".green2").removeClass('activegrin');
+			$(".green").removeClass('activegrin');
+			$(".orang").removeClass('activegrin');
+			$(".orang2").removeClass('activegrin');
+			$(".orang3").removeClass('activegrin');
+			$("#poslestroyBtn").removeClass('activegr');
+		}
+	});
+	$("#standartBtn").click(function(){
+		$(this).toggleClass("activegr");
+		if($(".slid1").hasClass("act")){
+			$(".blue").toggleClass("activegrin");
+			$(".blue2").removeClass('activegrin');
+			$(".blue3").removeClass('activegrin');
+			$(".orang").removeClass('activegrin');
+			$(".orang2").removeClass('activegrin');
+			$(".orang3").removeClass('activegrin');
+			$("#poslestroyBtn").removeClass('activegr');
+		}else if($(".slid2").hasClass("act")){
+			$(".blue2").toggleClass("activegrin");
+			$(".blue").removeClass('activegrin');
+			$(".blue3").removeClass('activegrin');
+			$(".orang").removeClass('activegrin');
+			$(".orang2").removeClass('activegrin');
+			$(".orang3").removeClass('activegrin');
+			$("#poslestroyBtn").removeClass('activegr');
+		}else if($(".slid3").hasClass("act")){
+			$(".blue3").toggleClass("activegrin");
+			$(".blue2").removeClass('activegrin');
+			$(".blue").removeClass('activegrin');
+			$(".orang").removeClass('activegrin');
+			$(".orang2").removeClass('activegrin');
+			$(".orang3").removeClass('activegrin');
+			$("#poslestroyBtn").removeClass('activegr');
+		}
+	});
+	$("#vipBtn").click(function(){
+		$(this).toggleClass("activegr");
+		if($(".slid1").hasClass("act")){
+			$(".red").toggleClass("activegrin");
+			$(".red2").removeClass('activegrin');
+			$(".red3").removeClass('activegrin');
+			$(".orang").removeClass('activegrin');
+			$(".orang2").removeClass('activegrin');
+			$(".orang3").removeClass('activegrin');
+			$("#poslestroyBtn").removeClass('activegr');
+		}else if($(".slid2").hasClass("act")){
+			$(".red2").toggleClass("activegrin");
+			$(".red").removeClass('activegrin');
+			$(".red3").removeClass('activegrin');
+			$(".orang").removeClass('activegrin');
+			$(".orang2").removeClass('activegrin');
+			$(".orang3").removeClass('activegrin');
+			$("#poslestroyBtn").removeClass('activegr');
+		}else if($(".slid3").hasClass("act")){
+			$(".red3").toggleClass("activegrin");
+			$(".red2").removeClass('activegrin');
+			$(".red").removeClass('activegrin');
+			$(".orang").removeClass('activegrin');
+			$(".orang2").removeClass('activegrin');
+			$(".orang3").removeClass('activegrin');
+			$("#poslestroyBtn").removeClass('activegr');
+		}
+	});
+	$("#poslestroyBtn").click(function(){ // отдельные оранживые кнопки
+		$(this).toggleClass("activegr");
+		if($(".slid1").hasClass("act")){
+			$(".orang").toggleClass("activegrin");
+			$(".orang2").removeClass('activegrin');
+			$(".orang3").removeClass('activegrin');
+			$(".red").removeClass('activegrin');
+			$(".red2").removeClass('activegrin');
+			$(".red3").removeClass('activegrin');
+			$(".blue").removeClass('activegrin');
+			$(".blue2").removeClass('activegrin');
+			$(".blue3").removeClass('activegrin');
+			$(".green").removeClass('activegrin');
+			$(".green2").removeClass('activegrin');
+			$(".green3").removeClass('activegrin');
+			$("#expresBtn").removeClass('activegr');
+			$("#standartBtn").removeClass('activegr');
+			$("#vipBtn").removeClass('activegr');
+
+		}else if($(".slid2").hasClass("act")){
+			$(".orang2").toggleClass("activegrin");
+			$(".orang").removeClass('activegrin');
+			$(".orang3").removeClass('activegrin');
+			$(".red").removeClass('activegrin');
+			$(".red2").removeClass('activegrin');
+			$(".red3").removeClass('activegrin');
+			$(".blue").removeClass('activegrin');
+			$(".blue2").removeClass('activegrin');
+			$(".blue3").removeClass('activegrin');
+			$(".green").removeClass('activegrin');
+			$(".green2").removeClass('activegrin');
+			$(".green3").removeClass('activegrin');
+			$("#expresBtn").removeClass('activegr');
+			$("#standartBtn").removeClass('activegr');
+			$("#vipBtn").removeClass('activegr');
+		}else if($(".slid3").hasClass("act")){
+			$(".orang3").toggleClass("activegrin");
+			$(".orang2").removeClass('activegrin');
+			$(".orang").removeClass('activegrin');
+			$(".red").removeClass('activegrin');
+			$(".red2").removeClass('activegrin');
+			$(".red3").removeClass('activegrin');
+			$(".blue").removeClass('activegrin');
+			$(".blue2").removeClass('activegrin');
+			$(".blue3").removeClass('activegrin');
+			$(".green").removeClass('activegrin');
+			$(".green2").removeClass('activegrin');
+			$(".green3").removeClass('activegrin');
+			$("#expresBtn").removeClass('activegr');
+			$("#standartBtn").removeClass('activegr');
+			$("#vipBtn").removeClass('activegr');
+		}
+	});
+
+//Каруселька
+	//Документация: http://owlgraphic.com/owlcarousel/
+	var owl = $(".carousel");
+	owl.owlCarousel({
+		items : 1,
+		dots: true,
+		center: true
+
+
+	});
+	owl.on("mousewheel", ".owl-wrapper", function (e) {
+		if (e.deltaY > 0) {
+			owl.trigger("owl.prev");
+		} else {
+			owl.trigger("owl.next");
+		}
+		e.preventDefault();
+	});
+	$(".next_button").click(function(){
+		owl.trigger("owl.next");
+	});
+	$(".prev_button").click(function(){
+		owl.trigger("owl.prev");
+	});
 	//Попап менеджер FancyBox
 	//Документация: http://fancybox.net/howto
 	//<a class="fancybox"><img src="image.jpg" /></a>
@@ -47,27 +263,7 @@ $(document).ready(function() {
 		});
 	});
 
-	//Каруселька
-	//Документация: http://owlgraphic.com/owlcarousel/
-	var owl = $(".carousel");
-	owl.owlCarousel({
-		items : 3,
-		
-	});
-	owl.on("mousewheel", ".owl-wrapper", function (e) {
-		if (e.deltaY > 0) {
-			owl.trigger("owl.prev");
-		} else {
-			owl.trigger("owl.next");
-		}
-		e.preventDefault();
-	});
-	$(".next_button").click(function(){
-		owl.trigger("owl.next");
-	});
-	$(".prev_button").click(function(){
-		owl.trigger("owl.prev");
-	});
+
 
 	//Кнопка "Наверх"
 	//Документация:
@@ -96,4 +292,16 @@ $(document).ready(function() {
 		return false;
 	});
 
+});
+
+jQuery(document).ready(function($){
+	//portfolio - show link
+	$('.bg-hover').hover(
+		function () {
+			$(this).animate({opacity:'1'});
+		},
+		function () {
+			$(this).animate({opacity:'0'});
+		}
+	);
 });
